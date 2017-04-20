1e62e90a2d6f8954c017dad3ebe5f2f17bdbc6a6
Andrew Day <Andrew Day>
2017-04-20 16:24:41 +1000

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</ Link>
+            </li>
+
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <p className="menu-text">Created by <a href="http://www.basicgaming.com.au" target="_blank">Andrew Day</a></p>
+        </div>
+      </div>
+    );
+  }
+})
+
+module.exports = Nav;
