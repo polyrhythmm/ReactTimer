9058171c99b89f62fcbaa55210a767aba6d8d34d
Andrew Day <Andrew Day>
2017-04-25 15:04:32 +1000

    Add styles to countdown page

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var CountDown = React.createClass({
   render: function(){
     return (
       <div>
         <h1>Count down</h1>
-        <p>This is the countdown page.</p>
+        <Clock totalSeconds={129}/>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = {
           Nav: 'app/components/Nav.jsx',
           Timer: 'app/components/Timer.jsx',
           CountDown: 'app/components/CountDown.jsx',
-          applicationStyles: 'app/styles/app.scss'
+          applicationStyles: 'app/styles/app.scss',
+          Clock: 'app/components/Clock.jsx'
         },
         extensions: ['','.js','.jsx']
     },
